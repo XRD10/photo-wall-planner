23faf501cd6a4cf6382ad145695c74c6ab10711d
James Cullen <94792906+JcullenNZ@users.noreply.github.com>
2024-10-03 23:45:05 +0200

    Implemented portrait and landscape. Different sized prefabs implemented.

--- photo-wall-planner/Assets/photo-wall-planner/scripts/FramePlacer.cs
+++ photo-wall-planner/Assets/photo-wall-planner/scripts/FramePlacer.cs
@@ -9,17 +9,22 @@ public class FramePlacer : PressInputBase
 {
     [SerializeField] private ARRaycastManager raycastManager;
     [SerializeField] private GameObject objectToPlace;
+    [SerializeField] private FrameMenuUI frames;
     private static readonly List<ARRaycastHit> _hits = new();
     private bool _framePlaced;
+
+
 
     protected override void OnPressBegan(Vector3 position)
     {
         base.OnPressBegan(position);
         if (_framePlaced) return;
         if (EventSystem.current.IsPointerOverGameObject()) return;
+        objectToPlace = frames.GetFrame();
         if (!raycastManager.Raycast(position, _hits, TrackableType.PlaneWithinPolygon)) return;
         var hitpose = _hits[0].pose;
-        Instantiate(objectToPlace, hitpose.position, hitpose.rotation);
+        // Get the main camera's position
+        Instantiate(objectToPlace, hitpose.position,objectToPlace.transform.rotation);
         _framePlaced = true;
     }
 }

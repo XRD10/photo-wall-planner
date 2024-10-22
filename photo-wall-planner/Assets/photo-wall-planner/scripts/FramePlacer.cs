edfcd60f564e639026ad765e18d9fdf8e69f4353
Simon <simon.mayer@lego.com>
2024-10-22 11:56:05 +0200

    fixed positioning and scale issues

--- photo-wall-planner/Assets/photo-wall-planner/scripts/FramePlacer.cs
+++ photo-wall-planner/Assets/photo-wall-planner/scripts/FramePlacer.cs
@@ -35,19 +35,17 @@ public class FramePlacer : PressInputBase
 
         if (Physics.Raycast(ray, out RaycastHit hitObject))
             if (hitObject.transform.CompareTag("Placable")) return;
-        if(objectToPlace == null)
+
+        if (objectToPlace == null)
         {
-            Debug.LogError("No object selected");
+            Debug.Log("No object selected");
             return;
         }
 
         PlaceFrame();
-        
     }
     public void PlaceFrame()
     {
-        //from cm to unity units (m)
-
         var hitpose = _hits[0].pose;
 
         GameObject instance = Instantiate(objectToPlace, hitpose.position, Quaternion.identity);
@@ -58,7 +56,7 @@ public class FramePlacer : PressInputBase
         float yRotation = frames.GetLandscape() ? 0f : 90f;
         instance.transform.Rotate(0, yRotation, 0, Space.Self);
         instance.tag = "Placable";
-       
+
     }
 
     public void PlaceCustomFrame(float sizeX, float sizeZ)
@@ -67,14 +65,14 @@ public class FramePlacer : PressInputBase
         sizeX /= 100;
         sizeZ /= 100;
 
-        var hitpose = _hits[0].pose;
+        Debug.Log("Placing custom frame with size: " + sizeX + "x" + sizeZ);
 
-        
+        var hitpose = _hits[0].pose;
 
         GameObject instance = Instantiate(objectToPlace, hitpose.position, hitpose.rotation);
         instance.transform.localScale = new Vector3(sizeX, objectToPlace.transform.localScale.y / 10, sizeZ);
+        instance.tag = "Placable";
         SetText(instance, sizeZ, sizeX);
-        
     }
 
     private void SetText(GameObject frame, float x, float y)
@@ -82,33 +80,36 @@ public class FramePlacer : PressInputBase
         if (frame == null) return;
 
         Vector3 parentScale = frame.transform.lossyScale;
-        Vector3 inverseScale = new Vector3((x / parentScale.x)*4, 4f, y / parentScale.z);
+        Vector3 inverseScale = new Vector3(x / parentScale.x * 4, 4f, y / parentScale.z);
 
         GameObject xText = new GameObject("XText");
-
-        xText.transform.SetParent(frame.transform,false);
+        xText.layer = LayerMask.NameToLayer("UI");
+        xText.transform.SetParent(frame.transform, false);
 
         TextMeshPro xTextMesh = xText.AddComponent<TextMeshPro>();
-        xTextMesh.text = (x*100).ToString() +"cm";
+        xTextMesh.text = (x * 100).ToString() + "cm";
         xTextMesh.color = Color.red;
         xTextMesh.fontSize = 0.1f;
         xTextMesh.alignment = TextAlignmentOptions.Center;
 
         xText.transform.localScale = inverseScale;
-        xText.transform.localPosition = new Vector3(0.08f, 0.22f, 0);
-        xText.transform.localRotation = Quaternion.Euler(90,-90,0);
-        
+        float xValue = frame.transform.localScale.x / 2;
+        float zValue = frame.transform.localScale.z / 2;
+        xText.transform.localPosition = new Vector3(xValue - 0.01f, 3, 0);
+        xText.transform.localRotation = Quaternion.Euler(90, -90, 0);
+
         GameObject yText = new GameObject("YText");
+        yText.layer = LayerMask.NameToLayer("UI");
         yText.transform.SetParent(frame.transform, false);
 
         TextMeshPro yTextMesh = yText.AddComponent<TextMeshPro>();
-        yTextMesh.text = (y*100).ToString() + "cm";
+        yTextMesh.text = (y * 100).ToString() + "cm";
         yTextMesh.color = Color.red;
         yTextMesh.fontSize = 0.1f;
-        yTextMesh.alignment= TextAlignmentOptions.Center;
+        yTextMesh.alignment = TextAlignmentOptions.Center;
 
         yText.transform.localScale = inverseScale;
-        yText.transform.localPosition = new Vector3(0, 0.22f, -0.08f);
+        yText.transform.localPosition = new Vector3(0, 3, -xValue + 0.01f);
         yText.transform.localRotation = Quaternion.Euler(90, -90, 90);
 
     }
